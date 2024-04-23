bfdb7a12fc69d50a508b2d7fb4d04889f01e2147
yeisonreyes2024 <161230156+yeisonreyes2024@users.noreply.github.com>
2024-04-22 21:09:42 -0500

    commit inicial

---
+++ javascript/login.js
@@ -0,0 +1,50 @@
+
+
+document.getElementById('loginForm').addEventListener('submit',function (event){
+  event.preventDefault()
+    const email= document.getElementById("email").value
+    const pass= document.getElementById("password").value
+    // console.log(e,p);
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    
+    const raw = JSON.stringify({
+      "email": email,
+      "password": pass
+    });
+    
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow"
+    };
+    
+    fetch("http://localhost:3000/login", requestOptions)
+      .then((response) => {
+          console.log('response',response)
+          if(response.status == 400){
+            Swal.fire({
+                title: 'No se encontró el usuario',
+                icon:"warning",
+                showConfirmButton:true
+            })
+          }else{
+            Swal.fire({
+                title: 'Ingreso exitoso',
+                text:"Bienvenido a Homeservice",
+                icon:"success",
+                showConfirmButton:true
+            })
+            // window.location.href="http://127.0.0.1:5500/frontend-homeService/index.html"
+            window.location.href="http://127.0.0.1:5500/frontend-homeService/vistas/dashboard.html"
+          }
+         
+          return response.text();
+    })
+    .then((result) => {
+       
+        
+    })
+    .catch((error) => console.error(error));
+})
---
+++ javascript/registro.js
@@ -0,0 +1,60 @@
+
+
+  document.getElementById('registroForm').addEventListener('submit',function (event){
+    event.preventDefault()
+    const email= document.getElementById("email").value
+    const pass= document.getElementById("password").value
+    const apellidos= document.getElementById("apellidos").value
+    const nombres= document.getElementById("nombre").value
+    const direccion= document.getElementById("direccion").value
+    const celular= document.getElementById("telefono").value
+    const identificacion= document.getElementById("identificacion").value
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    
+    const raw = JSON.stringify({
+      "correo": email,
+      "contrasenia": pass,
+      "nombre": nombres,
+      "apellido": apellidos,
+      "identificacion": identificacion,
+      "celular": celular,
+      "direccion": direccion
+
+    });
+    
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow"
+    };
+
+    
+    fetch("http://localhost:3000/registrar", requestOptions)
+      .then((response) => {
+          console.log('response',response)
+          if(response.status == 400){
+            Swal.fire({
+                title: 'No se pudo registrar el usuario',
+                icon:"warning",
+                showConfirmButton:true
+            })
+          }else{
+            Swal.fire({
+                title: 'Registro exitoso',
+                text:"usuario ha sido registrado correctamente",
+                icon:"success",
+                showConfirmButton:true
+            })
+            window.location.href="http://127.0.0.1:5500/frontend-homeService/vistas/login.html"
+          }
+         
+          return response.text();
+    })
+    .then((result) => {
+       console.log('result',result);
+        
+    })
+    .catch((error) => console.error(error));
+})

4595d2eb5f67fd41dbf2d3262aff77f5cdbf877c
yeisonreyes2024 <161230156+yeisonreyes2024@users.noreply.github.com>
2024-04-29 18:47:12 -0500

    actual

--- javascript/login.js
+++ javascript/login.js
@@ -26,7 +26,7 @@ document.getElementById('loginForm').addEventListener('submit',function (event){
                     showConfirmButton:true,
                 }).then(res=>{
                   if(res.isConfirmed){
-                    window.location.href="/vistas/dashboard.html"
+                    window.location.href="https://frontend-homeservice.vercel.app/vistas/dashboard.html"
                     return response.text();
     
                   }
@@ -58,7 +58,7 @@ document.getElementById('loginForm').addEventListener('submit',function (event){
                     showConfirmButton:true,
                 }).then(res=>{
                   if(res.isConfirmed){
-                    window.location.href="/vistas/dashboard.html"
+                    window.location.href="https://frontend-homeservice.vercel.app/vistas/dashboard.html"
                     return response.text();
     
                   }
